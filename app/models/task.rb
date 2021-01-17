632104491b8baddf1986316c38cf0c32a34f93de
EC2 Default User <ec2-user@ip-172-31-28-43.ap-northeast-1.compute.internal>
2021-01-17 09:02:06 +0000

    完成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ApplicationRecord
 
 validates :title, presence: true
-# validates :start_time, presence: true
-# validates :end_time, presence: true
+validates :start_time, presence: true
+validates :end_time, presence: true
 validates :content, presence: true
+validate :start_end_check
+validate :end_check
 
+def start_end_check
+   errors.add(:end_time,"は開始日以降の日付で選択してください")  if start_time.to_i > end_time.to_i
+end
+def end_check
+    errors.add(:end_time,"は、本日以降の日付で選択してください")  if end_time.to_i < Time.now.to_i
+end
 end
